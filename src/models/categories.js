9f789c2b3ef02f859b4b3a5f032685313f457a8b
Xochil Squaglia <xochilsquaglia@yahoo.com>
2019-03-05 20:55:25 -0800

    adding cat/prod

--- src/models/categories.js
+++ src/models/categories.js
@@ -6,15 +6,21 @@ class Categories {
     }
 
     get(_id) {
+        let queryObject = _id ? {_id} : {};
+        return schema.find(queryObject);
     }
 
     post(record) {
+        let newRecord = new schema(record);
+        return newRecord.save();
     }
 
     put(_id, record) {
+        return schema.findByIdAndUpdate(_id, record, {new:true});
     }
 
     delete(_id) {
+        return schema.findByIdAndDelete(_id);
     }
 
 }
--- src/models/products.js
+++ src/models/products.js
@@ -3,6 +3,8 @@
 const uuid = require('uuid/v4');
 
 const schema = {
+    id: {required: true},
+    record: {required: true},
 };
 
 class Products {
@@ -12,20 +14,52 @@ class Products {
     }
 
     get(id) {
+        let response = id ? this.database.filter((record) => record.id === id) : this.database;
+        return Promise.resolve(response);
     }
 
     post(entry) {
+        entry.id = uuid();
+        let record = this.sanitize(entry);
+        if (record.id) {
+            this.database.push(record);
+        }
+        return Promise.resolve(record);
     }
 
     put(id, entry) {
+        let record = this.sanitze(entry);
+        if (record.id) {
+            this.database = this.database.map((item) => (item.id === id) ? record : item);
+        }
+        return Promise.resolve(record);
     }
 
     delete(id) {
+        this.database = this.database.filter((record) => record.id !== id);
+        return Promise.resolve();
     }
 
     sanitize(entry) {
-    }
 
+        let valid = true;
+        let record = {};
+
+        Object.keys(schema).forEach(field => {
+            if (schema[field].required) {
+                if (entry[field]) {
+                    record[field] = entry[field];
+                } else {
+                    valid = false;
+                }
+            } else {
+                record[field] = entry[field];
+            }
+        });
+
+        return valid ? record : undefined;
+    }
 }
+
 
 module.exports = Products;
